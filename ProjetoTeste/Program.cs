a3674e68b77b64bc811cb5e0f0e0379e746bcee4
Niverton Felipe <nivertonfelipe@hotmail.com>
2019-08-16 16:42:29 -0300

    Acrescentada a mensagem de despedida

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -9,6 +9,7 @@ namespace ProjetoTeste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje? ");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau");
             
         }
     }
