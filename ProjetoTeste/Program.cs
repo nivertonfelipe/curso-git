a70cf0ba5693bcebffc431c8a13c3dcc1db417c2
Niverton Felipe <nivertonfelipe@hotmail.com>
2019-08-16 15:31:38 -0300

    Acrescentada a perguntada sobre como voce estah

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -8,7 +8,8 @@ namespace ProjetoTeste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
-
+            Console.WriteLine("Como você está hoje? ");
+            
         }
     }
 }
